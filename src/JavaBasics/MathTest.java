5f384b4f87528933021a60a0162e21ec39e099d5
Sekhar Kolli <Sekhar.Kolli@gmail.com>
2019-01-10 00:07:44 -0500

    Updated comments of files

--- src/JavaBasics/MathTest.java
+++ src/JavaBasics/MathTest.java
@@ -1,21 +1,27 @@
+/*
+ * Purpose : Demos the use of Math class
+ *
+ * Date: 10-January-2019
+ */
+
 package JavaBasics;
 
 // import java.lang.Math.*;
 
-//Static Imports. From Java 5 
+// Static Imports. From Java 5
 import static java.lang.Math.*;
 
 public class MathTest {
 
 	public static void main(String[] args) {
 
-//		System.out.println(Math.PI);
-//		System.out.println(Math.E);
-//		System.out.println(Math.sqrt(25));
-//		System.out.println(Math.sin(0));
-//		System.out.println(Math.cos(0));
-//		System.out.println(Math.ceil(10.1));
-//		System.out.println(Math.floor(10.1));
+// System.out.println(Math.PI);
+// System.out.println(Math.E);
+// System.out.println(Math.sqrt(25));
+// System.out.println(Math.sin(0));
+// System.out.println(Math.cos(0));
+// System.out.println(Math.ceil(10.1));
+// System.out.println(Math.floor(10.1));
 
 		// B'cos of static imports the above Math.* statements can be written in the
 		// following way
@@ -30,5 +36,5 @@ public class MathTest {
 	}
 
 	// Importing System package in static mode can help us use System.out.println(floor(10.1)); as out.println(floor(10.1));
-	
+
 }
