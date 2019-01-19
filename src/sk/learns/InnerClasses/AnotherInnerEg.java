9cc9abd03cc13f6fa142092bd7b9b070d42c41bf
Sekhar Kolli <Sekhar.Kolli@gmail.com>
2019-01-18 22:11:16 -0500

    A simple example of Inner classes

--- src/sk/learns/InnerClasses/AnotherInnerEg.java
+++ src/sk/learns/InnerClasses/AnotherInnerEg.java
@@ -19,8 +19,10 @@ class ClassOuter {
 
 public class AnotherInnerEg {
 	public static void main(String args[]) {
+		
 		ClassOuter o1 = new ClassOuter();
 		ClassOuter.ClassInner i1 = o1.new ClassInner();
+		
 		i1.display(); // will print 300 as it is accessible
 	}
 }
